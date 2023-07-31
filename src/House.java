17213ce915a2e15051fe01c47c4cf0bab4d65267
fabio <fabiocostanza91@gmail.com>
2023-07-31 21:31:44 +0200

    Exercise 16

---
+++ src/House.java
@@ -0,0 +1,10 @@
+ public class House {
+
+        String address;
+        int numberOfFloors;
+
+        House() {
+            System.out.println("A new House object has been created!");
+        }
+    }
+
